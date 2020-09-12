e301f1b97129ca13ffe7e2191dd95a36240ac690
SELeenux <43295067+SELeenux@users.noreply.github.com>
2020-09-13 00:01:16 +0900

    edit db.js urls.

--- src/passport.js
+++ src/passport.js
@@ -4,22 +4,11 @@ import FacebookStrategy from "passport-facebook";
 import User from "./models/User";
 import routes from "./routes";
 import { githubLoginCallback, facebookLoginCallback } from "./controllers/userController";
-// import dotenv from "dotenv";
-// import path from "path";
-
-// require('dotenv').config({ path: path.join(__dirname, "src/.env") });
-// require('dotenv').config();
-// dotenv.config({ path: path.join(__dirname, "src/.env") });
-// dotenv.config();
-
-console.log(process.env);
 
 passport.use(User.createStrategy());
 
 passport.use(
   new GithubStrategy({
-    // clientID: "59d44893c2af94fbd3e4",
-    // clientSecret: "c0389933d41f49c0d3a36b80525092f9e1dfc3e8",
     clientID: process.env.GH_ID,
     clientSecret: process.env.GH_SECRET,
     callbackURL: `http://localhost:4000${routes.githubCallback}`
@@ -31,8 +20,8 @@ passport.use(
 passport.use(
   new FacebookStrategy(
     {
-      clientID: "651755138781006",
-      clientSecret: "7fa2d736e2c8f20fd4a7cad2b985a7d3",
+      clientID: process.env.FB_ID,
+      clientSecret: process.env.FB_SECRET,
       callbackURL: `http://localhost:4000${routes.facebookCallback}`,
       profileFields: ["id", "displayName", "photos", "email"],
       scope: ["public_profile", "email"]
